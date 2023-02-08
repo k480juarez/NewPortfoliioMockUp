145ca414116098a3b834335cafb02975eb414929
k480juarez <k480juarez@gmail.com>
2023-02-08 09:46:26 -0700

    added 2 labels and 2 inputs for email and password

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+
+
+// import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from "react";
 
 function App() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  function onSubmit(e) {
+    e.preventDefault()
+    console.log({ email, password })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form onSubmit={onSubmit}>
+      <label htmlFor="email">Email</label>
+      <input
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        type="email"
+        id='email'
+        />
+        <label htmlFor='password'>Password</label>
+        <input
+          value={password}
+          onChange={e => setPassword}
+          />
+    </form>
+  )
+
 }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
